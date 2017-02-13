e7284e15b2d7655070afd1c858c9d0b23a7b8d30
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-13 17:05:42 -0600

    studio 4

---
+++ studios/studio4/SerialComm.java
@@ -0,0 +1,47 @@
+package studio4;
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+public class SerialComm {
+
+	SerialPort port;
+	boolean debug = true;
+	
+	public SerialComm(String name) throws SerialPortException {
+		port = new SerialPort(name);
+		port.openPort();
+		port.setParams(SerialPort.BAUDRATE_9600, 
+				SerialPort.DATABITS_8, 
+				SerialPort.STOPBITS_1, 
+				SerialPort.PARITY_NONE);
+	}
+	
+	public boolean available() throws SerialPortException {
+		return port.getInputBufferBytesCount() != 0;
+	}
+	
+	public byte readByte() throws SerialPortException {
+		byte[] byteArray = port.readBytes(1);
+		
+		if (debug) {
+			System.out.print("[0x" + String.format("%02x", byteArray[0]) + "]");
+		}
+		
+		return byteArray[0];
+	}
+	
+	public static void main(String[] args) throws SerialPortException {
+		SerialComm comm = new SerialComm("COM3");
+		
+		while (true) {
+			if (comm.available()) {
+				byte byte1 = comm.readByte();
+				byte byte2 = comm.readByte();
+				System.out.println((int) ((byte1 << 8) | byte2));
+			}
+			
+		}
+	}
+	
+}
---
+++ studios/studio4/datatypetest/datatypetest.ino
@@ -0,0 +1,15 @@
+// Name(s): Seunghan Bae
+
+void setup() {
+  Serial.begin(9600);
+
+  for(int i = 0; i < 360; i++) {
+    Serial.write((i >> 8) & 0xff);
+    Serial.write(i & 0xff);
+  }
+}
+
+void loop() {
+  
+}
+
