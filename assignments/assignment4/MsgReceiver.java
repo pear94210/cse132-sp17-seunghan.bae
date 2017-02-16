3825290f8cef1048131b5a2f9b875d112a41f7ff
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-16 01:15:58 -0600

    lab4

--- assignments/assignment4/MsgReceiver.java
+++ assignments/assignment4/MsgReceiver.java
@@ -11,11 +11,24 @@ public class MsgReceiver {
 		port = new SerialComm(portname);
 	}
 
-	public void run() {
+	public void run() throws SerialPortException {
 		// insert FSM code here to read msgs from port
 		// and write to console
-
-
+		while (true) {
+			if (port.available() && port.readByte() == 0x21) {
+				if (port.readByte() == 0x30) {
+					int byte1 = port.readByte();
+					int byte2 = port.readByte();
+					if (byte2 < 0) byte2 += 256;
+					int stringLength = (byte1 << 8) | byte2;
+					
+					for (int i = 0; i < stringLength; i++) {
+						System.out.print((char)port.readByte());
+					}
+					System.out.println();
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) throws SerialPortException {
--- assignments/assignment4/sender/sender.ino
+++ assignments/assignment4/sender/sender.ino
@@ -5,12 +5,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.write(0x21);
-  Serial.write(0x30);
+  sendDebug("java");
+  sendDebug("arduino");
+  sendDebug("hello world");
 }
 
-void sendDebug() {
+void sendDebug(String s) {
   Serial.write(0x21);
   Serial.write(0x30);
+  Serial.write((s.length() >> 8) & 0xff);
+  Serial.write(s.length() & 0xff);
+  for (int i = 0; i < s.length(); i++) {
+    Serial.write(s[i]);
+  }
 }
 
