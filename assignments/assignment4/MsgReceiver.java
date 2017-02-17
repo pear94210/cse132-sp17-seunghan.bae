4c1ddb640eae14f5d5176748e8bfed3dc24067ee
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-17 02:17:37 -0600

    lab 4

--- assignments/assignment4/MsgReceiver.java
+++ assignments/assignment4/MsgReceiver.java
@@ -15,18 +15,106 @@ public class MsgReceiver {
 		// insert FSM code here to read msgs from port
 		// and write to console
 		while (true) {
-			if (port.available() && port.readByte() == 0x21) {
-				if (port.readByte() == 0x30) {
-					int byte1 = port.readByte();
-					int byte2 = port.readByte();
-					if (byte2 < 0) byte2 += 256;
-					int stringLength = (byte1 << 8) | byte2;
+			if (port.available()) {
+				if (port.readByte() == 0x21) {
+					byte byteType = port.readByte();
 					
-					for (int i = 0; i < stringLength; i++) {
-						System.out.print((char)port.readByte());
-					}
-					System.out.println();
-				}
+					if (byteType == 0x30) {
+						System.out.print("Debug String");
+						int byte1 = port.readByte();
+						int byte2 = port.readByte();
+						if (byte2 < 0) byte2 += 256;
+						int stringLength = (byte1 << 8) | byte2;
+						
+						String byteString = "";
+						for (int i = 0; i < stringLength; i++) {
+							byte c = port.readByte();
+							if (c >= 0x01 && c <= 0x7f) {
+								byteString = byteString + (char)c;
+							} else System.out.println("WRONG CHARACTER");
+						}
+						
+						System.out.println(byteString);
+					} else if (byteType == 0x32) {
+						System.out.print("4-byte Integer");
+						int longLength = port.readByte();
+						if (longLength == 4) {
+							int byteInt = 0;
+							for (int i = 0; i < longLength; i++) {
+								int n = port.readByte();
+								if (n < 0) n += 256;
+								byteInt = (byteInt << 8) | n;
+							}
+							
+							System.out.println(byteInt);
+						} else System.out.println("CAN ONLY SEND 4 BYTES");
+
+					} else if (byteType == 0x33) {
+						System.out.print("Potentiometer Reading");
+						int intLength = port.readByte();
+						if (intLength == 2) {
+							int byteInt = 0;
+							for (int i = 0; i < intLength; i++) {
+								int n = port.readByte();
+								if (n < 0) n += 256;
+								byteInt = (byteInt << 8) | n;
+							}
+							
+							System.out.println(byteInt);
+						} else System.out.println("CAN ONLY SEND 2 BYTES");
+						
+					} else if (byteType == 0x34) {
+						System.out.print("Raw Temperature Reading");
+						int intLength = port.readByte();
+						if (intLength == 2) {
+							int byteInt = 0;
+							for (int i = 0; i < intLength; i++) {
+								int n = port.readByte();
+								if (n < 0) n += 256;
+								byteInt = (byteInt << 8) | n;
+							}
+							
+							System.out.println(byteInt);
+						} else System.out.println("CAN ONLY SEND 2 BYTES");
+						
+					} else if (byteType == 0x35) {
+						System.out.print("Unfiltered Temperature Reading");
+						int tempLength = port.readByte();
+						if (tempLength == 4) {
+							int byteTemp = 0;
+							for (int i = 0; i < tempLength; i++) {
+								int n = port.readByte();
+								if (n < 0) n += 256;
+								byteTemp = (byteTemp << 8) | n;
+							}
+							
+							int s = ((byteTemp >> 31) == 0) ? 1 : -1;
+							int e = ((byteTemp >> 23) & 0xff);
+							int m = (e == 0) ? (byteTemp & 0x7fffff) << 1 : (byteTemp & 0x7fffff) | 0x800000;
+							
+							System.out.println((float)s * m * Math.pow(2, e - 150));
+						} else System.out.println("CAN ONLY SEND 4 BYTES");
+						
+					} else if (byteType == 0x36) {
+						System.out.print("Filtered Temperature Reading");
+						int tempLength = port.readByte();
+						if (tempLength == 4) {
+							int byteTemp = 0;
+							for (int i = 0; i < tempLength; i++) {
+								int n = port.readByte();
+								if (n < 0) n += 256;
+								byteTemp = (byteTemp << 8) | n;
+							}
+							
+							int s = ((byteTemp >> 31) == 0) ? 1 : -1;
+							int e = ((byteTemp >> 23) & 0xff);
+							int m = (e == 0) ? (byteTemp & 0x7fffff) << 1 : (byteTemp & 0x7fffff) | 0x800000;
+							
+							System.out.println((float)s * m * Math.pow(2, e - 150));
+						} else System.out.println("CAN ONLY SEND 4 BYTES");
+						
+					} else System.out.println("WRONG FORMAT OF DATA");
+				} else System.out.println("MESSAGE MUST START WITH '!'");
 			}
 		}
 	}
--- assignments/assignment4/sender/sender.ino
+++ assignments/assignment4/sender/sender.ino
@@ -1,13 +1,43 @@
 // Name(s): Seunghan Bae
 
+const unsigned long deltaPeriod = 1000;
+const unsigned long tempPeriod = 250;
+unsigned long deltaTime = 0;
+unsigned long tempTime = 0;
+
+#define FILTER_COUNTS 8
+float temperatures[FILTER_COUNTS];
+int count = 0;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  sendDebug("java");
-  sendDebug("arduino");
-  sendDebug("hello world");
+  unsigned long now = millis();
+
+  //sendDebug("java");
+  //sendDebug("arduino");
+  //sendDebug("hello world");
+  
+  if (now - deltaTime >= deltaPeriod) {
+    unsigned long nowTime = now;
+    sendMillis(nowTime);
+    int pot = analogRead(0);
+    sendPotentiometer(pot);
+
+    deltaTime += deltaPeriod;
+  }
+
+  if (now - tempTime >= tempTPeriod) {
+    int temp = analogRead(1);
+    readTemp(temp);
+    sendTempAD(temp);
+    sendTempUnfiltered(temp);
+    sendTempFiltered(temp);
+    
+    tempTime += tempPeriod;
+  }
 }
 
 void sendDebug(String s) {
@@ -20,3 +50,73 @@ void sendDebug(String s) {
   }
 }
 
+void sendMillis(unsigned long l) {
+    Serial.write(0x21);
+    Serial.write(0x32);
+    Serial.write(0x04);
+    Serial.write((l >> 24) & 0xff);
+    Serial.write((l >> 16) & 0xff);
+    Serial.write((l >> 8) & 0xff);
+    Serial.write(l & 0xff);
+}
+
+void sendPotentiometer(int pot) {
+  Serial.write(0x21);
+  Serial.write(0x33);
+  Serial.write(0x02);
+  Serial.write((pot >> 8) & 0xff);
+  Serial.write(pot & 0xff);
+}
+
+void sendTempAD(int temp) {
+  Serial.write(0x21);
+  Serial.write(0x34);
+  Serial.write(0x02);
+  Serial.write((temp >> 8) & 0xff);
+  Serial.write(temp & 0xff);
+}
+
+void sendTempUnfiltered(int temp) {
+  float tempUnfiltered = temperatures[(count-1) % FILTER_COUNTS];
+  unsigned long rawBits = *(unsigned long *) &tempUnfiltered;
+  
+  Serial.write(0x21);
+  Serial.write(0x35);
+  Serial.write(0x04);
+  Serial.write((rawBits >> 24) & 0xff);
+  Serial.write((rawBits >> 16) & 0xff);
+  Serial.write((rawBits >> 8) & 0xff);
+  Serial.write(rawBits & 0xff);
+}
+
+void sendTempFiltered(int temp) {
+  float tempFiltered = getAvgTemp();
+  unsigned long rawBits = *(unsigned long *) &tempFiltered;
+  
+  Serial.write(0x21);
+  Serial.write(0x36);
+  Serial.write(0x04);
+  Serial.write((rawBits >> 24) & 0xff);
+  Serial.write((rawBits >> 16) & 0xff);
+  Serial.write((rawBits >> 8) & 0xff);
+  Serial.write(rawBits & 0xff);
+}
+
+void readTemp(int reading) {
+  float temperature = (reading * 110.0 / 1023) - 50;
+  temperatures[count % FILTER_COUNTS] = temperature;
+  count += 1;
+}
+
+float getAvgTemp() {
+  float tempSum = 0;
+  for (int i = 0; i < FILTER_COUNTS; i++) {
+    tempSum += temperatures[i];
+  }
+  
+  if (count < FILTER_COUNTS) {
+    return tempSum / count;
+  } else {
+    return tempSum / FILTER_COUNTS;
+  }
+}
