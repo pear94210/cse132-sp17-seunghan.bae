95a678076c83a15fabe843613a8fbc64057c7f78
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-22 13:35:20 -0600

    lab 5 prep

--- assignments/assignment5/MorseCoder/MorseCoder.ino
+++ assignments/assignment5/MorseCoder/MorseCoder.ino
@@ -1,6 +1,8 @@
 #include"MorseCodes.h"
 
 const int led = 13;
+const unsigned long morsePeriod = 500;
+unsigned long morseTime = 0;
 
 // Argument: Any character
 // Return Value: Either:
@@ -22,13 +24,34 @@ void convertIncomingCharsToMorseCode() {
   // TODO
   if (Serial.available()) {
     char input = Serial.read();
-    char* morse = morseEncode(input);
-    Serial.print(morse);
+    Serial.print(input);
+    Serial.print(": ");
+    String morse = morseEncode(input);
+    Serial.println(morse);
+    ledMorse(morse);
   }
 }
 
-
-
+void ledMorse(String morse) {
+  if (morse == " ") {
+    delay(2000);
+  } else {
+    for (int i = 0; i < morse.length(); i++) {
+      if (morse[i] == '.') {
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+      } else {
+        digitalWrite(led, HIGH);
+        delay(1500);
+        digitalWrite(led, LOW);
+        delay(500);
+      }
+    }
+    delay(1000);
+  }
+}
 
 void loop() {
   // No Need to Modify this.  Put most of your code in "convertIncomingCharsToMorseCode()"
---
+++ assignments/assignment5/SerialOutput.java
@@ -0,0 +1,32 @@
+package assignment5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import jssc.SerialPortException;
+import studio4.SerialComm;
+
+public class SerialOutput {
+	
+	public static void main(String[] args) throws SerialPortException, IOException {
+		
+		//SerialComm port = new SerialComm("COM3");
+		
+		while (true) {
+			System.out.println("Enter input:");
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String input = in.readLine();
+			
+			System.out.println(input);
+			
+//			for (int i = 0; i < input.length(); i++) {
+//				port.writeByte((byte)input.charAt(i));
+//			}
+//			System.out.println();
+		}
+		
+	}
+	
+}
