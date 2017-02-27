b57a85ad9ee925af1b61aa66559787d77cd06f73
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-27 00:24:46 -0600

    studio 6 prep

---
+++ studios/studio6/Writeup.java
@@ -0,0 +1,53 @@
+package studio6;
+
+public class Writeup {
+
+	/*
+	 * 2.1 Number bases
+	 * 
+	 * 01011010 = 0x5a
+	 * 1111111100111100 = 0xff3c
+	 * 01100111 = 0x67
+	 * 
+	 * 0xfe = 11111110
+	 * 0x4321 = 
+	 * 0x20 = 
+	 * 
+	 * 0x132 = 302
+	 * 0xcd = 
+	 * 0x11 = 
+	 * 
+	 * 723 = 0x2d3
+	 * 100 = 
+	 * 50 =
+	 * 
+	 * 2.2 Integer and fixed-point representations
+	 * 
+	 * -1 = 11111111(two's complement), 10000001(sign-magnitude), 10000000(excess-127)
+	 * -2 = 
+	 * Most significant bit = -(2^128)
+	 * Least significant bit = 2^0 = 1
+	 * Q1.15 has 15 fractional bits
+	 * 
+	 * 2.3 Floating point representation
+	 * exponent of a float in Java/C
+	 * double in C/Java has _ bytes
+	 * 
+	 * 2.4 Strings
+	 * 
+	 * 3 C language
+	 * 
+	 * 4 Timing
+	 * 
+	 * 5 Analog inputs
+	 * 
+	 * 6 Digital outputs
+	 * 
+	 * 7 Digital inputs
+	 * 
+	 * 8 FSMs
+	 * 
+	 * 9 Streams and communication
+	 */
+	
+}
