ada94b1e9de24c073a05a7785a1bf55b53ad906e
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-03-05 20:37:04 -0600

    studio7 prep

---
+++ studios/studio7/debounce/debounce.ino
@@ -0,0 +1,27 @@
+// Name(s): Seunghan Bae
+
+int button = 12;
+int buttonState = LOW;
+int tempState = LOW;
+
+unsigned long debounceDelay = 100;
+unsigned long debounceTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  int reading - digitalRead(button);
+  if (reading != tempState) debounceTime = millis();
+
+  if (millis() - debounceTime >= debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      Serial.println(buttonState);
+    }
+  }
+
+  tempState = reading;
+}
---
+++ studios/studio7/mirror/mirror.ino
@@ -0,0 +1,22 @@
+// Name(s): Seunghan Bae
+
+int led = 13;
+int button = 12;
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  bool lastButton = digitalRead(button);
+  bool thisButton = digitalRead(button);
+  digitalWrite(led, !digitalRead(button));
+  if (thisButton == lastButton + 1) {
+    count++;
+    Serial.println(count);
+  }
+}
+
