e5fd0376b3524b1fe40598fcdcbe753f6beb7230
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-03-20 15:56:32 -0500

    fitbit prep

---
+++ assignments/assignment9/Fitbit1.java
@@ -0,0 +1,9 @@
+package assignment9;
+
+public class Fitbit1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ assignments/assignment9/fitbit/fitbit.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <SparkFun_MMA8452Q.h>
+
+// Name(s): Seunghan Bae
+
+MMA8425Q accel;
+
+void setup() {
+  Serial.begin(9600);
+  accel.init();
+}
+
+void loop() {
+  if (accel.available) {
+    accel.read();
+
+    // FIXME
+  }
+}
+
