88416eb0ce788ebec90332c67cd73e1054d910f0
seunghan <seunghan@seunghans-mbp.dhcp.wustl.edu>
2017-03-31 20:24:15 -0500

    updated

---
+++ assignments/assignment10/Fitbit2.java
@@ -0,0 +1,158 @@
+package assignment10;
+
+import java.nio.charset.StandardCharsets;
+
+import jssc.SerialPortException;
+import sedgewick.StdDraw;
+import studio4.SerialComm;
+
+public class Fitbit2 {
+
+	final private SerialComm port;
+	
+	public Fitbit2(String portname) throws SerialPortException {
+		port = new SerialComm(portname);
+	}
+	
+	public static void main(String[] args) throws SerialPortException {
+		
+		createChart();
+		
+		Fitbit2 fitbit = new Fitbit2("COM3");
+		fitbit.run();
+
+	}
+	
+	public void run() throws SerialPortException {
+		while (true) {
+			if (port.available()) {
+				if (port.readByte() == 0x23) {
+					byte byteType = port.readByte();
+					
+					switch (byteType) {
+					case 0x30:
+						System.out.print("Debug String: ");
+						int[] input30 = new int[2];
+						for (int i = 0; i < input30.length; i++) {
+							input30[i] = port.readByte();
+							if (input30[i] < 0) input30[i] += 256;
+						}
+						int stringLength = (input30[0] << 8) | input30[1];
+						
+						byte[] byte30 = new byte[stringLength];
+						for (int i = 0; i < byte30.length; i++) {
+							byte30[i] = port.readByte();
+						}
+						String byte30String = new String(byte30, StandardCharsets.UTF_8);
+						
+						System.out.println(byte30String);
+						break;
+						
+					case 0x31:
+						System.out.print("Error String: ");
+						int[] input31 = new int[2];
+						for (int i = 0; i < input31.length; i++) {
+							input31[i] = port.readByte();
+							if (input31[i] < 0) input31[i] += 256;
+						}
+						int errorLength = (input31[0] << 8) | input31[1];
+						
+						byte[] byte31 = new byte[errorLength];
+						for (int i = 0; i < byte31.length; i++) {
+							byte31[i] = port.readByte();
+						}
+						String byte31String = new String(byte31, StandardCharsets.UTF_8);
+						
+						System.out.println(byte31String);
+						break;
+						
+					case 0x32:
+						System.out.print("Step Counts: ");
+						int[] input32 = new int[4];
+						for (int i = 0; i < input32.length; i++) {
+							input32[i] = port.readByte();
+							if (input32[i] < 0) input32[i] += 256;
+						}
+						int byte32 = (input32[0] << 24) | (input32[1] << 16) | (input32[2] << 8) | input32[3];
+						
+						System.out.println(byte32);
+						break;
+
+					case 0x33:
+						System.out.print("Sleep Time: ");
+						int[] input33 = new int[4];
+						for (int i = 0; i < input33.length; i++) {
+							input33[i] = port.readByte();
+							if (input33[i] < 0) input33[i] += 256;
+						}
+						int byte33 = (input33[0] << 24) | (input33[1] << 16) | (input33[2] << 8) | input33[3];
+					
+						System.out.println(byte33);
+						break;
+
+					case 0x34:
+						System.out.print("Temperature: ");
+						int[] input34 = new int[4];
+						for (int i = 0; i < input34.length; i++) {
+							input34[i] = port.readByte();
+							if (input34[i] < 0) input34[i] += 256;
+						}
+						int byte34 = (input34[0] << 24) | (input34[1] << 16) | (input34[2] << 8) | input34[3];
+						float byte34Float = Float.intBitsToFloat(byte34);
+						
+						System.out.println(byte34Float);
+						break;
+						
+					case 0x35:
+						System.out.print("Total Runtime: ");
+						int[] input35 = new int[4];
+						for (int i = 0; i < input35.length; i++) {
+							input35[i] = port.readByte();
+							if (input35[i] < 0) input35[i] += 256;
+						}
+						int byte35 = (input35[0] << 24) | (input35[1] << 16) | (input35[2] << 8) | input35[3];
+						
+						System.out.println(byte35);
+						break;
+						
+
+					default:
+						System.out.println("UNDEFINED DATA TYPE");
+						break;
+					}
+				} else System.out.println("MESSAGE MUST START WITH #(0x23)");
+			}
+		}
+	}
+	
+	public static void createChart() {
+		StdDraw.setXscale(-10, 50);
+		StdDraw.setYscale(-5, 3);
+		
+		StdDraw.setPenRadius(StdDraw.getPenRadius() * 2);
+		StdDraw.line(0, -1, 0, 2);
+		StdDraw.line(0, 0, 45, 0);
+		
+		java.awt.Font font = StdDraw.getFont();
+		
+		StdDraw.setFont(new java.awt.Font(font.getName(), font.getStyle(), font.getSize() * 2));
+		StdDraw.text(20, 2.5, "FITBIT");
+		
+		StdDraw.setFont(new java.awt.Font(font.getName(), java.awt.Font.BOLD, font.getSize()));
+		StdDraw.text(-60, 0.5, "accelerometer value", 90);
+		StdDraw.text(300, -1.25, "time(ms)");
+		
+		StdDraw.setFont(font);
+		StdDraw.setPenRadius(StdDraw.getPenRadius() / 2);
+		for (double d = -0.5; d <= 1.5; d += 0.5) {
+			StdDraw.textRight(-1, d, String.valueOf(d));
+			StdDraw.line(0, d, 45, d);
+		}
+		
+		for (int i = 10; i <= 40; i += 10) {
+			StdDraw.text(i, -1.25, String.valueOf(i));
+			StdDraw.line(i, -1, i, 2);
+		}
+	}
+
+}
---
+++ assignments/assignment10/fitbit/fitbit.ino
@@ -0,0 +1,211 @@
+// Name(s): Seunghan Bae
+
+#include <Wire.h>
+#include <SparkFun_MMA8452Q.h>
+
+MMA8452Q accel;
+
+int switchB = 12;
+bool switchState = LOW;
+bool switchLastState = LOW;
+unsigned long switchDebTime = 0;
+
+int resetB = 11;
+bool resetState = LOW;
+bool resetLastState = LOW;
+unsigned long resetDebTime = 0;
+
+bool mode = true;
+int modeLed = 13;
+unsigned long stepCount = 0;
+unsigned long sleepCount = 0;
+unsigned long stepTime = 0;
+unsigned long sleepTime = 0;
+bool threshold = true;
+double compare[] = {0, 0, 0};
+double prev[] = {0, 0, 0};
+
+int tempSensor = 0;
+unsigned long deltaTime = 0;
+unsigned long deltaPeriod = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(switchB, INPUT_PULLUP);
+  pinMode(resetB, INPUT_PULLUP);
+  pinMode(modeLed, OUTPUT);
+  accel.init();
+
+  sendDebug("Fitbit");
+  sendDebug("Mark.I");
+  sendDebug("...buy Apple Watch");
+}
+
+void loop() {
+  switchMode();
+  digitalWrite(modeLed, mode);
+  reset();
+  
+  if (mode) countStep();
+  else countSleep();
+
+  unsigned long now = millis();
+  if (now - deltaTime >= deltaPeriod) {
+    
+    if (mode) {
+      sendStep();
+      if (stepCount < 0) sendError("NEGATIVE STEPS");
+    } else {
+      sendSleep();
+      if (sleepCount < 0) sendError("NEGATIVE SLEEP TIME");
+    }
+    
+    sendTemp();
+    sendRuntime(now);
+    deltaTime += deltaPeriod;
+  }
+}
+
+void switchMode() {
+  bool reading = digitalRead(switchB);
+  if (reading != switchLastState) switchDebTime = millis();
+
+  if (millis() - switchDebTime >= 50) {
+    if (reading != switchState) {
+      switchState = reading;
+      if (!switchState) {
+        mode = !mode;
+        if (mode) {
+          stepCount = 0;
+          stepTime = millis();
+        } else {
+          sleepCount = 0;
+          sleepTime = millis();
+        }
+      }
+    }
+  }
+
+  switchLastState = reading;
+}
+
+void reset() {
+  bool reading = digitalRead(resetB);
+  if (reading != resetLastState) resetDebTime = millis();
+
+  if (millis() - resetDebTime >= 50) {
+    if (reading != resetState) {
+      resetState = reading;
+      if (!resetState) {
+        if (mode) {
+          stepCount = 0;
+          stepTime = millis();
+        } else {
+          sleepCount = 0;
+          sleepTime = millis();
+        }
+      }
+    }
+  }
+
+  resetLastState = reading;
+}
+
+void countStep() {
+    if (accel.available()) {
+      accel.read();
+    
+      compare[0] = compare[1];
+      compare[1] = compare[2];
+      compare[2] = accel.cz;
+
+      if (compare[1] <  0.7) threshold = true;
+
+      if (threshold) {
+        if ((compare[1] > compare[0]) && (compare[1] > compare[2]) && (compare[1] > 1.2)) {
+          stepCount++;
+          threshold = false;
+        }
+      }
+    }
+}
+
+void countSleep() {
+  if (millis() - sleepTime >= 50) {
+    if (accel.available()) {
+      accel.read();
+
+      bool boolX = abs(accel.cx - prev[0]) < 0.1;
+      bool boolY = abs(accel.cy - prev[1]) < 0.1;
+      bool boolZ = abs(accel.cz - prev[2]) < 0.1;
+
+      if (boolX && boolY && boolZ) sleepCount += 50;
+
+      prev[0] = accel.cx;
+      prev[1] = accel.cy;
+      prev[2] = accel.cz;
+    }
+    sleepTime += 50;
+  }
+}
+
+void sendDebug(String s) {
+  Serial.write(0x23);
+  Serial.write(0x30);
+  Serial.write((s.length() >> 8) & 0xff);
+  Serial.write(s.length() & 0xff);
+  for (int i = 0; i < s.length(); i++) {
+    Serial.write(s[i]);
+  }
+}
+
+void sendError(String s) {
+  Serial.write(0x23);
+  Serial.write(0x31);
+  Serial.write((s.length() >> 8) & 0xff);
+  Serial.write(s.length() & 0xff);
+  for (int i = 0; i < s.length(); i++) {
+    Serial.write(s[i]);
+  }
+}
+
+void sendStep() {
+  Serial.write(0x23);
+  Serial.write(0x32);
+  Serial.write(stepCount >> 24);
+  Serial.write(stepCount >> 16);
+  Serial.write(stepCount >> 8);
+  Serial.write(stepCount);
+}
+
+void sendSleep() {
+  Serial.write(0x23);
+  Serial.write(0x33);
+  Serial.write(sleepCount >> 24);
+  Serial.write(sleepCount >> 16);
+  Serial.write(sleepCount >> 8);
+  Serial.write(sleepCount);
+}
+
+void sendTemp() {
+  int tempReading = analogRead(tempSensor);
+  float temp = (tempReading * 500.0 / 1023) - 50;
+  unsigned long longTemp = *(unsigned long *) &temp;
+
+  Serial.write(0x23);
+  Serial.write(0x34);
+  Serial.write(longTemp >> 24);
+  Serial.write(longTemp >> 16);
+  Serial.write(longTemp >> 8);
+  Serial.write(longTemp);
+}
+
+void sendRuntime(unsigned long runtime) {
+  Serial.write(0x23);
+  Serial.write(0x35);
+  Serial.write(runtime >> 24);
+  Serial.write(runtime >> 16);
+  Serial.write(runtime >> 8);
+  Serial.write(runtime);
+}
+
