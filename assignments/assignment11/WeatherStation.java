d8b765362c9e83ebf5a0969a5dbf554757b9ab52
seunghan <seunghan@seunghans-mbp.dhcp.wustl.edu>
2017-04-03 21:46:58 -0500

    lab11

--- assignments/assignment11/WeatherStation.java
+++ assignments/assignment11/WeatherStation.java
@@ -9,13 +9,11 @@ public class WeatherStation {
 
     public static void main(String[] args) throws Exception {
         // Create a new instance of Weather Station
-
-
+    	WeatherStation ws = new WeatherStation();
 
         // Based on the name of the instance created above, call xx.sendGet().
         // This will test to the function we'll be creating below.
-
-
+    	ws.sendGet();
 
     }
 
@@ -27,55 +25,44 @@ public class WeatherStation {
         // Since we only need the current data (currently) you can use the API to exclude all of the excess blocks (REQUIRED).
         // Instructions to do that are here: https://darksky.net/dev/docs/forecast
         // Test this new URL by pasting it in your web browser. You should only see the information about the current weather.
-
-
+    	String urlString = "https://api.darksky.net/forecast/c26405c49c44bb1af74e820709f12ee7/38.649196,-90.306099?exclude=minutely,hourly,daily,alerts,flags";
 
         // Create a new URL object with the URL string you defined above. Reference: https://docs.oracle.com/javase/7/docs/api/java/net/URL.html
-
-
+    	URL url = new URL(urlString);
 
         // Follow the instructions in the URL API to open this connection.
         // Cast this to a HttpURLConnection and save it in a new HttpURLConnection object.
-
-
+    	HttpURLConnection huc = (HttpURLConnection)url.openConnection();
 
         // Use the HttpURLConnection API to setup the HttpURLConnection you defined above.
         // Reference: https://docs.oracle.com/javase/7/docs/api/java/net/HttpURLConnection.html
         // Set the request method.
-
-
+    	huc.setRequestMethod("GET");
 
         // Set the request property "User-Agent" to the User-Agent you saw in Wireshark when you did the first exercise in studio.
         // Repeat the quick wireshark example if you've forgotten. It should be in the form "xxxxxxx/#.#"
-
-
+    	huc.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30\r\n");
 
         // To debug, get and print out the response code.
-
-
+    	System.out.println(huc.getResponseCode() + " " + huc.getResponseMessage());
 
         // The rest of the code should be much more familiar.
         // Create an InputStream that gets the input stream from our HttpURLConnection object.
-
-
+    	InputStream in = huc.getInputStream();
 
         // Wrap it in a DataInputStream
-
-
+    	DataInputStream dataIn = new DataInputStream(in);
 
         // Read a line and save it in a string
-
-
+    	String s = dataIn.readLine();
 
         // Close the InputStream
-
-
+    	in.close();
 
         // Using string manipulation tools, pull out the string between quotes after "icon:"
         // For example: "summary":"Clear","icon":"clear-day","nearestStormDistance":27
         // You should pull out JUST "clear-day"
-
-
+    	
 
         // You will set this char (in a switch statement) to one of the 5 types of weather. (Nothing TODO here)
         char weatherChar = '\0';
@@ -87,8 +74,7 @@ public class WeatherStation {
         // If the value if wind, set it to W
         // If the value is any of the clear ones, set it to S
         // If the value is any type of precipitation, set it to P
-
-
+        
 
         // Now you're ready to implement this into your past code to send it to the Arduino.
         // You also have to make a couple modifications to handle the switch location requests from Arduino.
